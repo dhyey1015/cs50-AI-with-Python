c3e931084e4260775b26a9e35f1eedd66a877e28
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-07-15 00:36:14 +0530

    done with the functions player, result, winner, terminal utility

--- week-0/tictactoe/tictactoe.py
+++ week-0/tictactoe/tictactoe.py
@@ -65,21 +65,49 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] is not EMPTY:
+            return row[0]                 #[EMPTY, EMPTY, EMPTY] checking this case to avoid index error
 
+    for col in range(3):                                           
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] is not EMPTY:
+            return board[0][col]                               #[EMPTY, EMPTY, EMPTY] checking this case to avoid index error
+    
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not EMPTY:
+        return board[0][0]                           #[EMPTY, EMPTY, EMPTY] checking this case to avoid index error
+    
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not EMPTY:
+        return board[0][2] 
+    
+    return None                          #[EMPTY, EMPTY, EMPTY] checking this case to avoid index error       
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) is not None:
+        return True
+    
+    for row in board:
+        if EMPTY in row:
+            return False
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    win =  winner(board)
+    if win == X:
+        return 1
+    
+    elif win == O:
+        return -1
+    
+    else:
+        return 0
 
 
 def minimax(board):
